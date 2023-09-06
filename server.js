923833ffbb4d30fa7bd67487fd2a82e933956faa
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-09-06 14:55:09 +0530

    local push

---
+++ server.js
@@ -0,0 +1,46 @@
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './typeDefs.js'
+import resolvers from './resolvers.js'
+import jwt from 'jsonwebtoken'
+import { WebSocketServer } from 'ws'
+import express from 'express'
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { expressMiddleware } from '@apollo/server/express4'
+
+
+const PORT = process.env.PORT || 5000
+
+// create express
+const app = express();
+
+const context = ({ req }) => {
+  const { authorization } = req.headers
+  if (authorization) {
+    const { userId } = jwt.verify(authorization, process.env.JWT_SECRET)
+    return { userId }
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+// create apollo server
+const apolloServer = new ApolloServer({ schema, context });
+
+// apply middleware
+await apolloServer.start();
+app.use('/graphql', expressMiddleware(apolloServer));
+
+
+const server = app.listen(PORT, () => {
+  // create the websocket server
+  const wsServer = new WebSocketServer({
+    server,
+    path: '/graphql',
+  });
+
+  //use the websocket server with graphql
+  useServer({ schema }, wsServer);
+
+  console.log(`Server is running at port ${PORT}`)
+  console.log("Apollo and Subscription server is up")
+});
